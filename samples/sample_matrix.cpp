245aa9d64212c781f1f446bb68d627ecd1378f67
Vladlen666 <90177886+Vladlen666@users.noreply.github.com>
2022-10-25 21:12:57 +0300

    Add files via upload

---
+++ samples/sample_matrix.cpp
@@ -0,0 +1,31 @@
+﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// Copyright (c) Сысоев А.В.
+//
+// Тестирование матриц
+
+
+#include <iostream>
+#include "tmatrix.h"
+//---------------------------------------------------------------------------
+
+int main()
+{
+  TMatrix<int> a(3), b(3), c(3);
+  int i, j;
+
+  setlocale(LC_ALL, "Russian");
+  cout << "Тестирование программ поддержки представления треугольных матриц"
+    << endl;
+  for (i = 0; i < 3; i++)
+    for (j = i; j < 3; j++)
+    {
+      a[i][j] = i * 10 + j;
+      b[i][j] = (i * 10 + j) * 100;
+    }
+  c = a+b;
+  cout << "Matrix a = " << endl << a << endl;
+  cout << "Matrix b = " << endl << b << endl;
+  cout << "Matrix c = a + b" << endl << c << endl;
+  return 0;
+}
